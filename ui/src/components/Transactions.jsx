4f61e0a555ebb178b8ca811fe704036ed6a195c1
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-26 11:03:22 +0200

    260422 after services

--- ui/src/components/Transactions.jsx
+++ ui/src/components/Transactions.jsx
@@ -1,7 +1,20 @@
+import React, { useContext } from 'react';
+import { TransactionContext  } from '../context/TransactionContext';
+
+import dummyData from '../utils/dummyData';
+
+
+
 
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
+  
   return (
-    <div>Transactions</div>
+    <div className='flex w-full justify-center items-center 2xl:pc-20 gradient-bg-transactions'>
+      <div className='flex flex-col md:p-12 py-12 px-4'>
+        
+      </div>
+    </div>
   )
 }
 
