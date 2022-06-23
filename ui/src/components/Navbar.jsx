42e188750fe88ae3a917859132f12635a9fa3f41
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-06-23 21:14:30 +0200

    navbar

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -3,32 +3,29 @@ import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { useState } from 'react';
 
-import logo from '../../images/logo.png';
-
-const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
-);
+import logo from '../../images/logoBridge.png';
 
 
 const Navbar = () => {
 
-  /* Set if the mobile navigator is open or not */
+  const Menu = () => (
+    <>
+        <div><a href="#Home">Home</a></div>
+        <div><a href="#Latest">Transactions</a></div>
+        <div><a href="#Footer">About me</a></div>
+    </>
+    )
+
   const [toggleMenu, setToggleMenu] =  useState(false);
-  /* End of Set if the mobile navigator is open or not */
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <img src={logo} alt="logo" className="w-48 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
-        ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
-      </ul>
+      <div className="text-white md:flex hidden flex-row justify-self-start  flex-initial mx-4 cursor-pointer justify-between w-80">
+        <Menu />
+      </div>
       {/* Mobile */}
       <div className="flex relative">
         {!toggleMenu && (
@@ -42,10 +39,8 @@ const Navbar = () => {
             className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
-            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-            )}
+            <div className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></div>
+            <Menu />
           </ul>
         )}
       </div>
